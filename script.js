56ac4afd58514b402218a455d4aff811a3c926b9
Manas verma <manasverma1357@gmail.com>
2025-05-06 16:10:12 +0530

    select all DOM elements

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const startBtn = document.getElementById("start-btn");
+    const restartBtn = document.getElementById("restart-btn");
+    const nextBtn = document.getElementById("next-btn");
+    const scoreDisplay = document.getElementById("score");
+    const questionContainer = document.getElementById("question-container");
+    const resultContainer = document.getElementById("result-container");
+    const choicesList = document.getElementById("choices-list");
+    const questionTextDisplay = document.getElementById("question-text");
+})
