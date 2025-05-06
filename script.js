0cf6db3e7933cf93fcfefc15cadbce053113c41b
Manas verma <manasverma1357@gmail.com>
2025-05-06 16:19:34 +0530

    add array of questions

--- script.js
+++ script.js
@@ -7,4 +7,37 @@ document.addEventListener("DOMContentLoaded", () => {
     const resultContainer = document.getElementById("result-container");
     const choicesList = document.getElementById("choices-list");
     const questionTextDisplay = document.getElementById("question-text");
+
+    const questions = [
+        {
+            question: "What does HTML stand for?",
+            choice: [
+              "Hyper Text Markup Language",
+              "Home Tool Markup Language",
+              "Hyperlinks and Text Markup Language",
+              "Hyper Tool Multi Language"
+            ],
+            answer: "Hyper Text Markup Language"
+          },
+          {
+            question: "Which of these is a programming language?",
+            choice: ["HTTP", "HTML", "Python", "WWW"],
+            answer: "Python"
+          },
+          {
+            question: "What symbol is used to start a comment in Python?",
+            choice: ["//", "#", "/*", "--"],
+            answer: "#"
+          },
+          {
+            question: "Which keyword is used to define a function in JavaScript?",
+            choice: ["func", "function", "def", "lambda"],
+            answer: "function"
+          },
+          {
+            question: "Which of these is used to style web pages?",
+            choice: ["HTML", "CSS", "SQL", "Python"],
+            answer: "CSS"
+          }
+    ]
 })
